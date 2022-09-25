91a2a82e97004140bdecd923860137c76efba8f6
hirm159 <hiro159m@gmail.com>
2022-09-25 22:33:24 +0900

    first commit

---
+++ src/main/java/com/hirm159/javalearningapp/JavaLearningAppApplication.java
@@ -0,0 +1,13 @@
+package com.hirm159.javalearningapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLearningAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLearningAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hirm159/javalearningapp/JavaLearningAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hirm159.javalearningapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLearningAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
